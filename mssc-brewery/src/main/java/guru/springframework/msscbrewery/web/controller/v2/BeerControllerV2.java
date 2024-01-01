2484853a28865efaf2aa30a488b1eef8b8adc455
pragatech <pragatheeswaran.a.c@outlook.in>
2024-01-01 16:38:43 +0530

    till method validation. - It will be commented out later.

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -5,15 +5,19 @@ import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Constraint;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Validated //help us to enable method level annotation. this will help us validate input parameters to the method.
+// Adding contraints (i.e. @NotNull) at params will help us enable validations
 @RequestMapping("/api/v2/beer/")
 @RestController
 public class BeerControllerV2 {
@@ -25,14 +29,14 @@ public class BeerControllerV2 {
 
     @GetMapping("{beerId}")
     public ResponseEntity<BeerDtoV2> getBeer(
-            @PathVariable UUID beerId
+            @NotNull @PathVariable UUID beerId
     ){
         return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<BeerDtoV2> handlePost(
-            @Valid @RequestBody BeerDtoV2 beerDto){
+            @Valid @NotNull @RequestBody BeerDtoV2 beerDto){
         BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/"+savedDto.getId().toString());
